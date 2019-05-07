4a2a55b1412d29db022fa140bcee0ab38ae92f18
kbbushman <k.bushman@yahoo.com>
2019-05-07 15:34:03 -0700

    Adds login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,7 @@ const router = express.Router();
 // Database
 const db = require('../models');
 
+// POST Register Route
 router.post('/register', async (req, res) => {
   const errors = [];
 
@@ -42,5 +43,38 @@ router.post('/register', async (req, res) => {
   }
 
 });
+
+
+// POST Login Route
+router.post('/login', async (req, res) => {
+  if (!req.body.email || !req.body.password) return res.status(400).json({status: 400, error: 'Please enter your email and password'});
+  
+  try {
+    // Find User by email
+    const foundUser = await db.User.findOne({email: req.body.email});
+
+    // If no user found, return error
+    if (!foundUser) return res.status(400).json({status: 400, error: 'Email or password is incorrect'});
+
+    // Compare Passwords
+    const passwordsMatch = bcrypt.compareSync(req.body.password, foundUser.password);
+
+    // Return error if password do not match
+    if (!passwordsMatch) return res.status(400).json({status: 400, error: 'Email or password is incorrect'});
+
+    // Create user session
+    req.session.currentUser = foundUser._id;
+
+    res.status(200).json({status: 200, currentUser: foundUser._id});
+
+  } catch(err) {
+    console.log(err);
+    
+    res.status(500).json({status: 500, error: 'Something went wrong. Please try again'});
+  }
+
+});
+
+
 
 module.exports = router;
